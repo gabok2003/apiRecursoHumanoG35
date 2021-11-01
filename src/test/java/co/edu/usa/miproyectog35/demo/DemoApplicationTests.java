9dafa6c22f85f57a15c3fc1815a01df597238f0c
gabok2003 <gabriel.fonseca.mt@correo.usa.edu.co>
2021-11-01 16:15:42 -0500

    Inicializacion Proyecto

---
+++ src/test/java/co/edu/usa/miproyectog35/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usa.miproyectog35.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
